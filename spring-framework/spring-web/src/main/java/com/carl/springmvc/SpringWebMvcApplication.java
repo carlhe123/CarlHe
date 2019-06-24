5891ea7b0d15d1fe13866f8a355cca9f9867976a
hebiao <he337612412@163.com>
2019-06-24 17:50:13 +0800

    更新OMC系统架构介绍

--- spring-framework/spring-web/src/main/java/com/carl/springmvc/SpringWebMvcApplication.java
+++ spring-framework/spring-web/src/main/java/com/carl/springmvc/SpringWebMvcApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-public class SpringMvcApplication {
+public class SpringWebMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcApplication.class, args);
+		SpringApplication.run(SpringWebMvcApplication.class, args);
 	}
 }
