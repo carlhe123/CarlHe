c472c6ce7a670ce74b051d2eb10c6c60903a1c38
Carl.hb <hebiao@hdfax.com>
2019-07-31 16:20:52 +0800

    增加zuul简单案例

---
+++ spring-cloud-framework/spring-cloud-zuul/src/main/java/com/carl/springcloudzuul/SpringCloudZuulApplication.java
@@ -0,0 +1,15 @@
+package com.carl.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableZuulServer
+public class SpringCloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulApplication.class, args);
+	}
+
+}
